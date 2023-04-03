57365789c422dcae7efbdd8a6ace59f81aebc1db
seol11796 <seol11796@gmail.com>
2023-04-03 09:44:11 +0900

    initialized

---
+++ cupid/src/main/java/com/sl/cupid/CupidApplication.java
@@ -0,0 +1,13 @@
+package com.sl.cupid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupidApplication.class, args);
+	}
+
+}
---
+++ cupid/src/test/java/com/sl/cupid/CupidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.cupid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
