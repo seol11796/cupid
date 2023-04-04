7f70c0841c65be47d33f017eb841a5e256b5141f
seol11796 <seol11796@gmail.com>
2023-04-04 11:02:33 +0900

    sign up function

---
+++ cupid/src/main/java/com/sl/cupid/boundedContext/member/entity/Member.java
@@ -0,0 +1,31 @@
+package com.sl.cupid.boundedContext.member.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    private String username;
+    private String password;
+}
